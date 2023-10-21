d58a7b53d3c7c887a2183cbc3f73118d394e51a5
ArsenalBotz <147291827+ArsenalBotz@users.noreply.github.com>
2023-10-21 09:01:49 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -5,7 +5,7 @@ from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-@Bot.on_message(filters.command('about') & filters.private & subscribed)
+@Bot.on_message(filters.command('about') & filters.private)
 async def start_command(client: Client, message: Message):
     await message.reply_photo(
             photo="https://graph.org/file/648bb4dba065accd53c5c.jpg",
