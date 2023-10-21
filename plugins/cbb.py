9551e573b24d1ebc68a2debe3942c0587e081255
ArsenalBotz <147291827+ArsenalBotz@users.noreply.github.com>
2023-10-21 08:58:45 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,6 +1,6 @@
 #(©)Codexbotz
 
-from pyrogram import __version__
+from pyrogram import Client, filters, __version__
 from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
