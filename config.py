ab6a3be07e76c897a8573958504c0fc60b48496f
ArsenalBotz <147291827+ArsenalBotz@users.noreply.github.com>
2023-10-27 17:38:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://arsenalbotz:arsenalbotz@a
 DB_NAME = os.environ.get("DATABASE_NAME", "arsenal")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001965820482"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001651419414"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
